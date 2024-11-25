3c732bcb2adf7cbd16d16bd20683afe872c0d01e
Oskar Enoksson <oskar@nordluft.se>
2024-11-25 02:04:32 +0100

    First attempt

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # adsb
 ADSB and Mode-S decoder/encoder
+
+Example usage: decoding messages given either as `bytes` or `int`:
+```
+from adsb import Adsb
+print(Adsb.parse(0x8D40621D58C382D690C8AC2863A7))
+print(Adsb.parse(b'\x8D\x48\x40\xD6\x20\x2C\xC3\x71\xC3\x2C\xE0\x57\x60\x98')
+```
+
+Example usage: tracking aircrafts:
+```
+from adsb import Tracker
+tracker = Tracker()
+for msg in readline():
+    m = Adsb.parse(msg.strip().encode())
+    tracker.process(m)
+```
+
---
+++ src/adsb/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright © 2024 Oskar Enoksson <enok@lysator.liu.se> - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the MIT license
+#
+# You should have received a copy of the MIT license with this file.
+# See also https://github.com/enok71/adsb/LICENSE
+#
+"""
+Self-contained functionality for encoding/decoding ADS-B messages
+Written in pure Python 3.
+"""
+__version__ = '0.0.1'
+
+from .adsb import Adsb
+from .tracker import Tracker
